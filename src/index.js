7b969e5bc1d7592466c2beda7c1f71bfa3c93a6b
Sam Levens <sjlevens@gmail.com>
2019-01-01 13:22:12 +1100

    Lifecycle implementations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    //super(props) -- reference to the parents constructor function nescesary
+    super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // THE ONLY TIME DIRECT ASSIGNMENT TO STATE
+    this.state = { lat: null, errorMessage: '' };
+  }
+
+  componentDidMount() {
+    console.log('Mounted');
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //Called setState which comes from React
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log('Updated');
+  }
+
+  // React needs to define render
+  render() {
+    console.log('rendering');
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
